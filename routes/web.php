e638d09196a290a86b812440b4c0fcb2a0ca2dbb
claudioLoaiciga <loaiciga38@gmail.com>
2020-05-16 19:23:36 -0600

    modulo configuracion usuario

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/configuracion', 'UserController@config')->name('config');
+Route::post('/user/update', 'UserController@update')->name('user.update');
